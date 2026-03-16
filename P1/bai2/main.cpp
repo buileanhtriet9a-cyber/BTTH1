9009e0f0d6e2546b8950af41575d6b2632615940
buileanhtriet9a-cyber <buileanhtriet.9a@gmail.com>
2026-03-16 17:27:22 +0700

    main.cpp

--- P1/bai2/main.cpp
+++ P1/bai2/main.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main(){
     int a,b,c,d;
     cin>>a>>b>>c>>d;
-    if (a*d>c*b)
+    if (a*d>c*b)/*Kiểm tra xem phân số nào lớn hơn*/
         cout<<a<<"/"<<b;
     else cout<<c<<"/"<<d;
     return 0;
 }
+/*Input: a=2 b=3 c=4 d=5
+  Output: 4/5
+  Hàm so sánh hai phân số xem số nào lớn nhất*/
